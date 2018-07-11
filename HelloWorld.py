d155a034a2fd3c507159d6388a04ae4acb137819
Adrian Deardorff <80deardorff@live.com>
2018-07-11 03:04:55 -0700

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,25 @@
+# print('Hello, World!')
+#
+# splitstring = 'This is a string\nthat is\nsplit on\nmany lines.'
+# print(splitstring)
+#
+# # TO COMMENT OUT CODE, USE A HASH TAG
+#
+# print("You can escape \"quotes\" by using this technique.")
+#
+# # FOR A TAB, YOU CAN WRITE THE FOLLOWING \t AFTER ANYTHING.
+# print('1\t2\t3\t4\t5\t')
+#
+# print("""You can also escape "quotes" by the triple quote techqique. Also this
+# goes to the
+# next line automatically when typed out this way.""")
+
+x = 8
+y = 7
+
+if x > y:
+    print('x is greater than y')
+elif x < y:
+    print('x is smaller than y')
+else:
+    print('x equals y')
